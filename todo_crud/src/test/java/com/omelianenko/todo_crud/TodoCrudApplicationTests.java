12ffb3dccaf6fb93b08e610d3eed4fc87ad84d43
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2026-01-09 21:02:48 +0300

    Initial commit

---
+++ todo_crud/src/test/java/com/omelianenko/todo_crud/TodoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omelianenko.todo_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
