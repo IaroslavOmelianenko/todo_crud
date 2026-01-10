77fd508c61c84ed6040ebecfa2e14dacb96b5021
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2026-01-10 15:40:39 +0300

    add main class, application yaml

--- src/main/java/com/omelianenko/todo_crud/TodoCrudApplication.java
+++ src/main/java/com/omelianenko/todo_crud/TodoCrudApplication.java
@@ -1,13 +1,34 @@
 package com.omelianenko.todo_crud;
 
+import com.omelianenko.todo_crud.model.Todo;
+import com.omelianenko.todo_crud.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoCrudApplication {
+public class TodoCrudApplication implements CommandLineRunner {
+
+	private TodoRepository todoRepository;
+
+	@Autowired
+	public void setTodoRepository(TodoRepository todoRepository) {
+		this.todoRepository = todoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoCrudApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		if (todoRepository.count() == 0) {
+			todoRepository.save(new Todo("Read doc", "become smarter"));
+			todoRepository.save(new Todo("Create todo app", "create app with CRUD operations"));
+
+			System.out.println("Test data added");
+		}
+	}
 }
